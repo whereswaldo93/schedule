1d5c4ca2b018d3d10fb595a10bc492a3c8aa06bb
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2018-12-28 17:50:07 -0700

    Used react animate height library to animate paragraph height

--- src/components/action.js
+++ src/components/action.js
@@ -3,27 +3,31 @@ import Icon from './icon';
 
 class Action extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.status = false;
+    }
+
     handleAction = function() {
         this.props.onClick()
-
-        // document.getElementById('action').classList.add('')
+        if(!this.status) {
+            document.getElementById(this.id).classList.add('action-remove');
+        } else {
+            document.getElementById(this.id).classList.remove('action-remove');
+        }
+        this.status = !this.status;     
     }.bind(this)
 
     render() {
+        this.id = `action ${this.props.id}`
         return (
             <a 
-                id = "action"
-                onClick={() => this.props.onClick()} 
+                id={this.id}
+                onClick={() => this.handleAction()} 
                 className={`${this.props.className} action`}>
             </a>
         )
-
-    // return (
-    //     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-    //         { Icon('fas fa-plus-circle') }
-    //         {/* fas fa-times-circle */}
-    //     </button>
-    // )
     }
 }
 
